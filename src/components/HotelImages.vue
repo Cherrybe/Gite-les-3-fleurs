31a941db679f274da4aa823962de7d8f384ab0a2
chrissy45 <117347389+Christabel-Quaye@users.noreply.github.com>
2025-04-09 14:01:48 +0000

    update image

--- src/components/HotelImages.vue
+++ src/components/HotelImages.vue
@@ -2,14 +2,8 @@
   <div class="flex gap-6 relative">
     <img 
       class="h-auto" 
-      src="../assets/images/topview.png" 
+      src="../assets/images/about.png" 
       alt="Hotel View"
-    >
-
-    <img 
-      class="h-auto translate-y-25" 
-      src="../assets/images/food.png" 
-      alt="Hotel Food"
     >
   </div>
 </template>
